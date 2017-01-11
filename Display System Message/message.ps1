cf6961805006b3b56d59d6ffb19c3afa513c4eb8
Steve <stbonner01@gmail.com>
2017-01-11 12:19:39 -0500

    Add files via upload

---
+++ Display System Message/message.ps1
@@ -0,0 +1,2 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+[System.Windows.Forms.MessageBox]::Show("Things, Stuff. Please reboot prior to using stuff. ", "S. Bonner")
