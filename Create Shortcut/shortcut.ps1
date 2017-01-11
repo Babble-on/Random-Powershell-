c0402ad6aec1e52091de5878c55f0175d531d4fe
Steve <stbonner01@gmail.com>
2017-01-11 12:23:52 -0500

    Add files via upload

---
+++ Create Shortcut/shortcut.ps1
@@ -0,0 +1,8 @@
+﻿$AppLocation = "Path to app"
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("location and name of .lnk")
+$Shortcut.TargetPath = $AppLocation
+$Shortcut.Description ="Safety Points"
+$Shortcut.WorkingDirectory ="path to app"
+$Shortcut.IconLocation = "location of icon.ico,0"
+$Shortcut.Save()
